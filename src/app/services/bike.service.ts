02a6b9cd8ffe22c2490331dda4761fd212941198
xb205500 <p000emoizinho@prservicos.com.br>
2019-11-25 01:25:54 -0300

    Implementing the service

--- src/app/services/bike.service.ts
+++ src/app/services/bike.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
 @Injectable()
 export class BikeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBikes(){
+    return this.http.get('/server/api/v1/bikes');
+  }
 
 }
