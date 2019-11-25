e2ae5035ff28d3c4fb5a5084870c50ac6d625e19
xb205500 <p000emoizinho@prservicos.com.br>
2019-11-25 01:27:30 -0300

    Creating a component to display the backend information through the service created previosly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
+import { BikeService } from './services/bike.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AdminComponent } from './components/admin/admin.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BikeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
